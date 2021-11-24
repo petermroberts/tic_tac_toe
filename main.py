ded2cb0442a2cc68a5ec471b64a79afd811064d2
petermroberts <peterroberts7998@gmail.com>
2021-11-24 13:40:12 -0400

    Fixed rechoosing position

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def choose_first_turn(player1, player2):
         return [player2, player1]
 
 def select_position(game_master, player):
-    chosen_position = int(input("Please choose where you want to place your marker (1-9)")) - 1
+    chosen_position = int(input("Please choose where you want to place your marker (1-9): ")) - 1
     while game_master.check_position(chosen_position):
-        int(input("That position is already filled, please choose again"))
+        chosen_position = int(input("That position is already filled, please choose again: "))
     game_master.place_marker(chosen_position, player.marker)
 
 def main():
@@ -24,7 +24,7 @@ def main():
     game_master = GameMaster()
 
     print("Welcome to tic tac toe!\n")
-    if input("Would you like to be X or O?").casefold == "x":
+    if input("Would you like to be X or O? ").casefold == "x":
         player1 = Player(1)
         player2 = Player(2)
     else:
@@ -37,7 +37,8 @@ def main():
         for player in players:
             select_position(game_master, player)
             game_master.print_board()
-            print(f"{game_master.check_win()} won!")
+            if game_master.check_win():
+                print(f"{game_master.check_win()} won!")
 
 if __name__ == "__main__":
     main()
