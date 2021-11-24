bbe5f65f1533b291614d80bfb57ebbe02574b8ce
petermroberts <peterroberts7998@gmail.com>
2021-11-24 13:28:55 -0400

    Now returns the marker string instead of a number

--- game_master.py
+++ game_master.py
@@ -1,6 +1,8 @@
 
 class GameMaster:
 
+    convert_dict = {0: " ", 1: "X", 2: "O"}
+
     def __init__(self):
         self.board = [0, 0, 0,
                       0, 0, 0,
@@ -17,10 +19,9 @@ class GameMaster:
 
     def print_board(self):
         x, y, z = 0, 1, 2
-        convert_dict = {0: " ", 1: "X", 2: "O"}
         for i in range(3):
             print("     |     |     ")
-            print(f"  {convert_dict[self.board[x]]}  |  {convert_dict[self.board[y]]}  |  {convert_dict[self.board[z]]}  ")
+            print(f"  {self.convert_dict[self.board[x]]}  |  {self.convert_dict[self.board[y]]}  |  {self.convert_dict[self.board[z]]}  ")
             print("     |     |     ")
             x += 3
             y += 3
@@ -32,35 +33,35 @@ class GameMaster:
 
         if self.board[0] == self.board[1] == self.board[2] != 0:
             self.game_over = True
-            return self.board[0]
+            return self.convert_dict[self.board[0]]
 
         elif self.board[3] == self.board[4] == self.board[5] != 0:
             self.game_over = True
-            return self.board[3]
+            return self.convert_dict[self.board[3]]
 
         elif self.board[6] == self.board[7] == self.board[8] != 0:
             self.game_over = True
-            return self.board[6]
+            return self.convert_dict[self.board[6]]
 
         elif self.board[0] == self.board[4] == self.board[8] != 0:
             self.game_over = True
-            return self.board[0]
+            return self.convert_dict[self.board[0]]
 
         elif self.board[2] == self.board[4] == self.board[6] != 0:
             self.game_over = True
-            return self.board[0]
+            return self.convert_dict[self.board[0]]
 
         elif self.board[0] == self.board[3] == self.board[6] != 0:
             self.game_over = True
-            return self.board[0]
+            return self.convert_dict[self.board[0]]
 
         elif self.board[1] == self.board[4] == self.board[7] != 0:
             self.game_over = True
-            return self.board[1]
+            return self.convert_dict[self.board[1]]
 
         elif self.board[2] == self.board[5] == self.board[8] != 0:
             self.game_over = True
-            return self.board[2]
+            return self.convert_dict[self.board[2]]
 
         else:
             return None
